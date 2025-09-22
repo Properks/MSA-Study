83a625be033057900b836a9b330caa1ed9a78683
Jeongmo Seo <wjdah1402@gmail.com>
2025-09-22 16:20:16 +0900

    feat(eureka-server): eureka server 생성 및 등록

---
+++ EurekaServer/src/main/kotlin/org/jeongmo/eurekaserver/security/SecurityConfig.kt
@@ -0,0 +1,44 @@
+package org.jeongmo.eurekaserver.security
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(
+    @Value("\${eureka.username}") private val username: String,
+    @Value("\${eureka.password}") private val password: String,
+) {
+
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf {it.disable()}
+            .httpBasic(Customizer.withDefaults())
+            .authorizeHttpRequests {it.anyRequest().authenticated()}
+        return http.build()
+    }
+
+    @Bean
+    fun userDetailsService(): UserDetailsService {
+        val user: UserDetails = User.builder()
+            .username(this.username)
+            .password(passwordEncoder().encode(this.password))
+            .roles("ADMIN")
+            .build()
+        return InMemoryUserDetailsManager(user)
+    }
+}
