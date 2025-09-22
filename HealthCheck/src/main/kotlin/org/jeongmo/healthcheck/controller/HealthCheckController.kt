55134e3097d78ba5d4ef5e0f5ef396d2d64e5098
Jeongmo Seo <wjdah1402@gmail.com>
2025-09-22 16:22:42 +0900

    fix(health-check): controller 위치 이동

--- HealthCheck/src/main/kotlin/org/jeongmo/healthcheck/controller/HealthCheckController.kt
+++ HealthCheck/src/main/kotlin/org/jeongmo/healthcheck/controller/HealthCheckController.kt
@@ -1,4 +1,4 @@
-package org.jeongmo.healthcheck
+package org.jeongmo.healthcheck.controller
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
